69c703e94027dd7c57869bd97fb4f4204a1e40e6
James H. Linder <git@jlinder.com>
2018-01-24 12:07:16 -0500

    Updating bootstrap.sh with pyenv and neovim setup.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,21 +9,41 @@ ln -s ~/dotfiles/bash/bashrc        ~/.bashrc
 ln -s ~/dotfiles/gitconfig          ~/.gitconfig
 ln -s ~/dotfiles/gitignore_global   ~/.gitignore_global
 ln -s ~/dotfiles/inputrc            ~/.inputrc
-ln -s ~/dotfiles/vimrc              ~/.vimrc
+ln -s ~/dotfiles/python-version     ~/.python-verison
 
 # install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install bash-completion \
+brew install \
+    bash-completion \
     docker-clean \
     git \
-    ipython \
     libpng \
     libyaml \
+    jq \
     neovim \
     node \
     openssl \
-    python \
-    python3 \
+    pyenv \
+    pyenv-virtualenv \
     readline \
     ruby
+
+# install vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+exit 0
+
+# Run this after the rest of the script
+
+pyenv install 2.7.14
+pyenv install 3.6.4
+pyenv virtualenv 3.6.4 neovim
+
+echo neovim > python-version
+
+pip install neovim --upgrade
+
+echo 3.6.4 > python-version
+
+pip install ipython
