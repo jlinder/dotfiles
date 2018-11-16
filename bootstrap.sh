96349eb1ac1b9a209bad7789f752c11b98291ed6
James H. Linder <git@jlinder.com>
2018-11-15 19:25:03 -0500

    Adding wget to the brew installed programs in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -29,7 +29,8 @@ brew install \
     pyenv-virtualenv \
     readline \
     ruby \
-    watch
+    watch \
+    wget
 
 # install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
